7fb9c3dfb9c2f3361355dd8657f6ac0c56567f7c
Chris Rabuse <rab233@live.com>
2016-08-23 19:01:18 -0500

    Add database model for pages

---
+++ src/server/models/page.ts
@@ -0,0 +1,15 @@
+import { Database } from '../modules/database';
+import { User } from './user';
+import * as sequelize from 'sequelize';
+
+let Page = Database.define('pages', {
+  title: sequelize.TEXT,
+  body: sequelize.TEXT,
+  url: sequelize.TEXT
+});
+
+Page.hasOne(User, { foreignKey: 'authorId'});
+
+Page.sync();
+
+export { Page };
