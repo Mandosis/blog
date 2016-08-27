80ca813c45687c24b0ce563a14ff57f43bfa98f4
Chris Rabuse <rab233@live.com>
2016-08-26 21:12:27 -0500

    Add user creation to setup

--- src/server/modules/setup.ts
+++ src/server/modules/setup.ts
@@ -3,42 +3,38 @@ import { User } from './models';
 
 const prompt = require('prompt');
 
-let questions = {
-  properties: {
-    name: {
-      pattern: /^[a-zA-Z\s\-]+$/,
-      message: 'Name must be only letters, spaces, or dashes',
-      description: 'Name displayed on blog posts',
-      required: true
-    },
-    email: {
-      name: 'email',
-      description: 'Admin email address',
-      format: 'email',
-      message: 'Must be a valid email address'
-    },
-    password: {
-      description: 'Admin password',
-      hidden: true,
-      required: true
-    },
-    confirmPassword: {
-      description: 'Confirm password',
-      hidden: true,
-      required: true
-    }
-  }
-}
-
 class Setup {
 
   private _email: string;
   private _password: string;
 
+  private _accountQuestions = {
+    properties: {
+      name: {
+        pattern: /^[a-zA-Z\s\-]+$/,
+        message: 'Name must be only letters, spaces, or dashes',
+        description: 'Name displayed on blog posts',
+        required: true
+      },
+      email: {
+        name: 'email',
+        description: 'Admin email address',
+        format: 'email',
+        message: 'Must be a valid email address'
+      },
+      password: {
+        description: 'Admin password',
+        hidden: true,
+        required: true
+      }
+    }
+  };
+
   /**
    * Bootstrap Setup
    */
   start() {
+
     console.log(`
       Welcome!
     `);
@@ -49,12 +45,30 @@ class Setup {
     prompt.message = '';
   	prompt.delimiter = ':';
 
-    prompt.get(questions, (err, result) => {
-      console.log(result);
+    prompt.get(this._accountQuestions, (err, result) => {
+      if (err) {
+        console.log(err);
+      } else {
+
+        User
+          .create({
+            email: result.email,
+            password: EncryptPassword(result.password),
+            name: result.name
+          })
+          .then(() => {
+            console.log('Admin user created successfully.');
+            process.exit();
+          })
+          .catch((err) => {
+            console.log(err);
+          });
+      }
+
+
     })
   };
 
-  
 
 }
 
