9a76af952b42658be27d472345d37370c4751831
Chris Rabuse <rab233@live.com>
2016-09-03 21:20:41 -0500

    Initial implementation of formatting controls for the editor

--- src/client/app/editor/editor.component.ts
+++ src/client/app/editor/editor.component.ts
@@ -18,8 +18,12 @@ import { SpeakingurlPipe } from '../speakingurl.pipe';
 
 export class EditorComponent {
   viewSettings: boolean = false;
+  isSelected: boolean = false;
+  selectionStart: number;
+  selectionEnd: number;
+  cursorPosition: number;
 
-  post: Object = {
+  post = {
     title: 'Welcome to the Editor',
     body: '# Hello'
   }
@@ -30,6 +34,51 @@ export class EditorComponent {
       this.viewSettings = false;
     } else {
       this.viewSettings = true;
+    }
+  }
+
+  getSelected(event: any) {
+
+
+    this.isSelected = true;
+
+    let editor = event.target;
+    this.selectionStart = editor.selectionStart;
+    this.selectionEnd = editor.selectionEnd;
+  }
+
+  setCursorPosition(event: any) {
+    this.isSelected = false;
+    let editor = event.target;
+
+    this.cursorPosition = editor.selectionStart;
+
+    console.log('Curosor Position:', this.cursorPosition);
+  }
+
+  setBold() {
+
+    console.log('isSelected', this.isSelected);
+
+    if (!this.isSelected) {
+      let beforeSelection = (this.post.body).substring(0, this.cursorPosition - 1);
+      let afterSelection = (this.post.body).substring(this.cursorPosition + 1, this.post.body.length);
+      let boldSyntax = '****';
+
+      let completedString = beforeSelection + boldSyntax + afterSelection;
+
+      this.post.body = completedString;
+    } else {
+      let beforeSelection = (this.post.body).substring(0, this.selectionStart - 1);
+      let afterSelection = (this.post.body).substring(this.selectionEnd + 1, this.post.body.length);
+      let selection = (this.post.body).substring(this.selectionStart, this.selectionEnd)
+
+      let boldSelection = '**' + selection + '**';
+
+      let completedString = beforeSelection + boldSelection + afterSelection;
+
+      this.post.body = completedString;
+
     }
   }
 }
